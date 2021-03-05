9edb3f5e6705b5639deb254b6745f83daa0b51a1
Vaibhav G <gvaibhav1734@gmail.com>
2021-03-06 03:00:07 +0530

    comments

--- new/network.sh
+++ new/network.sh
@@ -6,11 +6,15 @@ INTERACTIVE=''
 
 function networkUp {
   rm log.txt
+  # Spawn cli container to generate crypt stuff for CA's
   docker-compose -f $CLI_FILE up -d > log.txt 2>&1 --remove-orphans
   cat log.txt
+  # THe function which generates all crypt stuff for all CA's
   docker exec ${INTERACTIVE} cli  /bin/bash -c ". ./organizations/fabric-ca/registerEnroll.sh && createOrgs"
+  # Spawn all CA containers
   docker-compose -f $COMPOSE_FILE_CA up -d > log.txt 2>&1 --remove-orphans
   cat log.txt
+  # Register peer0 user "org admin" "peer0 msp" "certificates"
   docker exec ${INTERACTIVE} ca_centralgovernment /bin/bash -c ". organizations/fabric-ca/registerEnroll.sh && createCentralgovernment"
   docker exec ${INTERACTIVE} ca_stategovernmentfps /bin/bash -c ". organizations/fabric-ca/registerEnroll.sh && createStategovernmentfps"
   docker exec ${INTERACTIVE} ca_stategovernmentdepot /bin/bash -c ". organizations/fabric-ca/registerEnroll.sh && createStategovernmentdepot"
@@ -24,6 +28,8 @@ function networkDown {
   cat log.txt
   docker-compose -f $COMPOSE_FILE_CA down > log.txt 2>&1 --remove-orphans
   cat log.txt
+
+  # TODO: Add commands to remove the generated files
 }
 
 if [ "$1" == "up" ]; then
@@ -31,3 +37,5 @@ if [ "$1" == "up" ]; then
 elif [ "$1" == "down" ]; then
   networkDown
 fi
+
+# TODO: Add .gitignore to remove all generated fiels from git
